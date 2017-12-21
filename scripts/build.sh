e23d36f20ac3c92da040c72b3f33245c7f86e65c
Meg4R0M <psykoterro@gmail.com>
2017-12-21 06:03:26 +0100

    commit version 1.8-lua

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 set -ex
 
-BUILD_DEPS="make gcc libc-dev linux-headers python pcre-dev openssl-dev zlib-dev"
-RUN_DEPS="pcre libssl1.0 libcrypto1.0 zlib rsyslog openrc"
+BUILD_DEPS="make gcc libc-dev linux-headers python pcre-dev openssl-dev zlib-dev lua5.3-dev"
+RUN_DEPS="pcre libssl1.0 libcrypto1.0 zlib rsyslog openrc lua5.3-libs"
 
 if [ -z ${HAPROXY_MAJOR} ];
 	then HAPROXY_MAJOR="1.8";
@@ -18,13 +18,6 @@ fi
 
 apk upgrade --no-cache
 
-if [ -z "$WITH_LUA" ]; then
-	WITH_LUA=
-else
-	BUILD_DEPS="$BUILD_DEPS lua5.3-dev"
-	RUN_DEPS="$RUN_DEPS lua5.3-libs"
-fi
-
 # install build dependencies
 apk add --no-cache --virtual build-deps ${BUILD_DEPS}
 
@@ -39,7 +32,7 @@ make -C /usr/src/haproxy all \
 	USE_PCRE=1 USE_PCRE_JIT=1 \
 	USE_OPENSSL=1 \
 	USE_ZLIB=1 \
-	USE_LUA=$WITH_LUA LUA_LIB=/usr/lib/lua5.3/ LUA_INC=/usr/include/lua5.3
+	USE_LUA=1 LUA_LIB=/usr/lib/lua5.3/ LUA_INC=/usr/include/lua5.3
 
 # install haproxy
 make -C /usr/src/haproxy install-bin PREFIX=/usr TARGET=linux2628
